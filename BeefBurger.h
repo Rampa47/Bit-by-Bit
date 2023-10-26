4ac27d24ad0374f8f2c705eadfe910fdd89bc69d
Future <u22524798@tuks.co.za>
2023-10-26 15:48:30 +0200

    Beef burger class

---
+++ BeefBurger.h
@@ -0,0 +1,26 @@
+#ifndef BEEFBURGER_H
+#define BEEFBURGER_H
+
+#include "Burger.h"
+
+/**
+ * @class BeefBurger
+ * @brief Represents a beef burger, a specific type of burger.
+ */
+class BeefBurger : public Burger {
+public:
+
+    /**
+     * @brief Adds a beef patty to the burger.
+     *
+     * This implementation adds a beef patty to the burger.
+     */
+    void addPatty() override;
+
+    /**
+     * @brief Virtual destructor for the BeefBurger class.
+     */
+    virtual ~BeefBurger();
+};
+
+#endif
