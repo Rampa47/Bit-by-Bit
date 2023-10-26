1fdcc901a44aedddf5c2754b80a91e265f7de77e
Future <u22524798@tuks.co.za>
2023-10-26 16:01:24 +0200

    Grilled class

---
+++ GrilledPoultry.cpp
@@ -0,0 +1,15 @@
+#include "GrilledPoultry.h"
+#include <iostream>
+
+void GrilledPoultry::cook() {
+    /**
+     * @brief Cooks the grilled poultry.
+     */
+    std::cout << "Grilling the poultry..." << std::endl;
+}
+
+GrilledPoultry::~GrilledPoultry() {
+    /**
+     * @brief Virtual destructor for the GrilledPoultry class.
+     */
+}
