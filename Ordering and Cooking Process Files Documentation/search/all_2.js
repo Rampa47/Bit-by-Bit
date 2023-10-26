c5c6fc0b8f0d528709ce55ab145b5903a89de927
Future <u22524798@tuks.co.za>
2023-10-26 18:12:52 +0200

    updated Cooking and Ordering Documentation

---
+++ Ordering and Cooking Process Files Documentation/search/all_2.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['chefhandler_0',['ChefHandler',['../class_chef_handler.html',1,'']]],
+  ['cheftask_1',['ChefTask',['../class_chef_task.html',1,'']]],
+  ['chickenburger_2',['ChickenBurger',['../class_chicken_burger.html',1,'']]],
+  ['chips_3',['Chips',['../class_chips.html',1,'']]],
+  ['chipsauce_4',['chipsauce',['../class_chip_sauce.html',1,'ChipSauce'],['../class_chip_sauce.html#a8cad6bbeb3ffd1d99c6b96090861fec2',1,'ChipSauce::ChipSauce()']]],
+  ['cook_5',['cook',['../class_burger.html#a2554947cac06d74d3451ca5ab41f1189',1,'Burger::cook()'],['../class_chips.html#a987cd5c623efd07d64e2e63440990e49',1,'Chips::cook()'],['../class_chip_sauce.html#a14c94f69dd43c69d51df6d571e70944e',1,'ChipSauce::cook()'],['../class_food_item.html#a7c60667ee80bb9f434a5d5be861cc2c1',1,'FoodItem::cook()'],['../class_fried_poultry.html#a7397eff4573eb52057ac6c660fbf943c',1,'FriedPoultry::cook()'],['../class_grilled_poultry.html#a028ce98657f13db1ac553d8bfd2f278b',1,'GrilledPoultry::cook()'],['../class_jalapeno_chip_sauce.html#a4bf15eabea97d0abade8672a696322c0',1,'JalapenoChipSauce::cook()'],['../class_plain_chips.html#a2a9574ce063bd0dd8fe067a7b7cf4075',1,'PlainChips::cook()'],['../class_poultry.html#a9ca65fc64312133d6e836cec35268c53',1,'Poultry::cook()'],['../class_poultry_type.html#a22aa9b0fcb589256f98f8798e81a4032',1,'PoultryType::cook()'],['../class_tomato_chip_sauce.html#a1409ef8eacf3d1273fdc47787717c580',1,'TomatoChipSauce::cook()']]]
+];
