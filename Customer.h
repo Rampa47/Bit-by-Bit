0b858283718a54e5fadc5f67b96ddf67f0f57b4d
Future <u22524798@tuks.co.za>
2023-11-03 23:34:32 +0200

    made changes to customer folder

--- Customer.h
+++ Customer.h
@@ -1,7 +1,9 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 #include <string>
-
+#include <cstdlib>
+#include <array>
+#include "CustomerDisposition.h"
 #include "ChickenBurger.h"
 #include "BeefBurger.h"
 #include "PlainChips.h"
@@ -11,18 +13,40 @@
 #include "FriedPoultry.h"
 #include "Poultry.h"
 #include "FoodItem.h"
-#include "Order.h"
 
+
+
+class Order{};
+class Waiter{};
 class Customer
 {
     public:
         Customer();
         Customer(std::string name);
-        std::string getName();
+        std::string getName(); /**<Returns the name of a customer */
+
+        void setTableNumber(int tableNumber);
+        int getTableNumber() const;
+
+        std::shared_ptr<Order> order();
+        void expressMood(std::shared_ptr<Waiter> waiter);
+        void receiveOrder(std::shared_ptr<Order> order);
+        void eatFood();
         void selectFoodItems(Order * order);
 
+        //client is no longer a colleague 
+        // void receive(std::string to,std::string message);
+        // void send(std::string to,std::string message);
+
     private:
+        int tableNumber;
         std::string name;
+        std::shared_ptr<CustomerDisposition> disposition;
+        std::shared_ptr<Order> receivedOrder;
+        static std::array<std::string,40> names;
+        std::shared_ptr<CustomerDisposition> getRandomMood();
+
+
 };
 
 #endif
