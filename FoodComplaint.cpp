d384c3844306dceee2509981e636d967ac91c1ef
zdanise <zetdanise89@gmail.com>
2023-11-04 20:53:56 +0200

    my version

--- FoodComplaint.cpp
+++ FoodComplaint.cpp
@@ -3,16 +3,20 @@
 FoodComplaint::FoodComplaint(ComplaintsHandler* handler): Complaint(handler)
 {
 }
+FoodComplaint::FoodComplaint(ComplaintsHandler* handler,int degree)
+{
+
+}
 
 FoodComplaint::~FoodComplaint()
 {
 }
- FoodComplaint::FoodComplaint(ComplaintsHandler* handler,int degree){}
+ //FoodComplaint::FoodComplaint(ComplaintsHandler* handler,int degree){}
 
 void FoodComplaint::AddressComplaint(string complaint,int degree)
 {
     
     this->degree=degree;
-    cout << "Food Complaint: " <<  complaint << endl;
+    std::cout << "Food Complaint: " <<  complaint << std::endl;
     handler->handleComplaint("Food",degree);
 }
