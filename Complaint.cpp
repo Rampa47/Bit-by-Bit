d0a3b41f6227c7fff050c07f3ff26678e1f9aa42
leratokg <leratowork1331@gmail.com>
2023-11-04 07:09:38 +0200

    constructor parameters updated

--- Complaint.cpp
+++ Complaint.cpp
@@ -1,8 +1,9 @@
 #include"Complaint.h"
 
-Complaint::Complaint(ComplaintsHandler* handler)
+Complaint::Complaint(ComplaintsHandler* handler,int degree)
 {
    this->handler=handler;
+   this->degree=degree;
 }
 
 Complaint::~Complaint()
