0232e25052cdab3029346bfad44aeb5e7712d7eb
lwando04 <144447768+lwando04@users.noreply.github.com>
2023-11-06 18:21:21 +0200

    Added doxygen comments

--- AwaitingBill.cpp
+++ AwaitingBill.cpp
@@ -1,6 +1,16 @@
 #include "AwaitingBill.h"
 #include "ReadyToLeave.h"
 
+
+/**
+ * @brief Handles the behavior of a table in the "Awaiting Bill" state.
+ *
+ * This function handles the actions that a table in the "Awaiting Bill" state
+ * should perform, such as waiting for the bill, generating the bill receipt,
+ * and transitioning to the "Ready To Leave" state.
+ *
+ * @param table The table to be handled.
+ */
 void AwaitingBill::handle(Table& table)
 {
     std::cout << "Table "<<to_string(table.getTableNumber())<<" is waiting for the bill..." << std::endl;
@@ -11,24 +21,37 @@ void AwaitingBill::handle(Table& table)
     std::cout<<"Table "<<to_string(table.getTableNumber())<<" Receipt: "<<endl;
     std::cout<<bill->getBillReceipt();
     ThreadSleep::threadSleep();
-    if (bill->isTab()){
+    if (bill->isTab())
+    {
         std::cout << "  [Table "<<to_string(table.getTableNumber())<<" has opened a tab...]" << std::endl;
     }
-    else{
+    else
+    {
         std::cout << "  [Table "<<to_string(table.getTableNumber())<<" has payed the bill...]" << std::endl;
     }
     delete bill;
-    //instruct waiter to send the bill to the table
-    //table.setBill(table.getWaiter().getBill());
 }
 
+/**
+ * @brief Returns the next state after handling the current state.
+ *
+ * This function returns the next state, which is "Ready To Leave," as the
+ * table transitions from "Awaiting Bill" to "Ready To Leave."
+ *
+ * @return A pointer to the "ReadyToLeave" state.
+ */
 TableState* AwaitingBill::getNextState()
 {
     return new ReadyToLeave();
-    
 }
 
-
+/**
+ * @brief Returns a string representation of the "Awaiting Bill" state.
+ *
+ * This function returns a string that represents the name of the state.
+ *
+ * @return The string "Awaiting Bill."
+ */
 std::string AwaitingBill::toString()
 {
     return "Awaiting Bill";
--- AwaitingBill.h
+++ AwaitingBill.h
@@ -1,10 +1,15 @@
+/**
+ * @file AwaitingBill.h
+ */
+
 #ifndef AWAITINGBILL_H
 #define AWAITINGBILL_H
 
 #include "TableState.h"
 
 
-class AwaitingBill: public TableState{
+class AwaitingBill: public TableState
+{
     public:
         virtual void handle(Table& table);
         virtual TableState* getNextState();
