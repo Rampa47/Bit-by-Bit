1c079698dcf7c2b8cb3c63da803e361ed4d79851
Khanyestics <132166766+Khanyestics@users.noreply.github.com>
2023-11-07 02:29:41 +0200

    Update Waiter.h

--- Waiter.h
+++ Waiter.h
@@ -29,23 +29,47 @@ private:
 
 
     public:
-        Waiter(int wNum, ChefHandler* chef);
-        void handleComplaint(std::string c, int degree);
-        void takeOrder(Order* order);
+     /**
+     * @brief Constructor for the Waiter class.
+     * @param wNum Waiter number.
+     * @param chef Pointer to the ChefHandler.
+     */
+     Waiter(int wNum, ChefHandler* chef);
+
+    /**
+     * @brief Handles various complaints reported by customers.
+     * @param c The type of complaint.
+     * @param degree The severity level of the complaint.
+     */
+    void handleComplaint(std::string c, int degree);
+
+/**
+ * @brief Takes the customer's order and processes it.
+ * 
+ * This function initiates the order-taking process for the customer. 
+ * It interacts with the customer to receive their order and communicates
+ * with the kitchen to process the order items. It sets up an Order object
+ * to manage the items requested by the customer and updates the bill amount
+ * accordingly.
+ * 
+ * @param order Pointer to the Order object containing the customer's request.
+ */
+void takeOrder(Order* order);
           /**
          * @brief Receives message from Mediator .
          * @param to Specifies to who the message is for.
          * @param message The actual message that will be received by the receiver.
          */
-        void receive(std::string to, std::string message);
+void receive(std::string to, std::string message);
          /**
          * @brief Will allow Colleague to send a message to a specific Colleague through the Mediator.
          */
-        void send() override; 
+void send() override; 
         /**
          * @brief Returns the concrete Colleagues name.
          */
-        std::string getClassname();
+    std::string getClassname();
+
         void greetCustomer();
         void updateBill(double price, int quantity);
         void presentBill();
@@ -57,6 +81,11 @@ private:
         void addNext(ComplaintsHandler* c);
         void setTable(Table * t);
         void checkOnTable();
+
+ /**
+     * @brief Destructor for the Waiter class.
+     */
+    
         ~Waiter();
        
         
